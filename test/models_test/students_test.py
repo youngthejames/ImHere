54ad62c434f7f3adef6946ce8ac811608ffc5882
Matt Mallett <mmallett91@gmail.com>
2016-11-10 00:29:38 -0500

    more robust testing of students get courses, keep making stupid typos, going to bed..

--- test/models_test/students_test.py
+++ test/models_test/students_test.py
@@ -1,4 +1,5 @@
 from models import students_model
+from models import courses_model
 
 def test_get_uni(db):
     sm = students_model.Students(db, 3)
@@ -10,6 +11,23 @@ def test_get_courses(db):
 
     dave_courses = dave.get_courses()
     assert len(dave_courses) == 1
+    art = dave_courses[0]
+    assert art['cid'] == 4
+    assert art['name'] == 'Art History'
+
+    cm_running = courses_model.Courses(db, 1)
+    cm_running.add_student('ds9876')
+
+    dave_courses = dave.get_courses()
+    assert len(dave_courses) == 2
+    running = dave_courses[0]
+    assert running['cid'] == 1
+    assert running['name'] == 'Running'
+    art = dave_courses[1]
+    assert art['cid'] == 4
+    assert art['name'] == 'Art History'
+
+    dave_courses = dave.get_courses()
 
     jaina = students_model.Students(db, 6)
 
