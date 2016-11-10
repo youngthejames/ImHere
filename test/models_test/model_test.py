bf8cdc47e8c4d1a124253028887316b88b44b140
Matt Mallett <mmallett91@gmail.com>
2016-11-09 23:57:29 -0500

    add test for sql string escape in model

--- test/models_test/model_test.py
+++ test/models_test/model_test.py
@@ -1,4 +1,6 @@
 import sqlalchemy
+import pytest
+
 from models import model
 
 def test_deproxy(db):
@@ -13,3 +15,21 @@ def test_deproxy(db):
     assert type(deproxy_list) is list
     assert type(deproxy_list[0]) is sqlalchemy.engine.result.RowProxy
     assert len(deproxy_list) == 1
+
+def test_escape_string(db):
+    model_obj = model.Model(db)
+
+    query = "select * from users where email = '{0}'"
+
+    safe = model_obj.escape_string("matt@brogrammers.co.nr")
+    danger = model_obj.escape_string("dave '; drop table users;")
+
+    try:
+        db.execute(query.format(safe))
+    except:
+        pytest.fail('query syntax' + safe)
+
+    try:
+        db.execute(query.format(danger))
+    except:
+        pytest.fail('query syntax' + danger)
