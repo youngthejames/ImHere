a129dce899a8122d6e7b92e52fa25e4d4676eecf
Matt Mallett <mmallett91@gmail.com>
2016-11-10 00:05:41 -0500

    add a couple clauses to teacher, student detection in index model test

--- test/models_test/index_test.py
+++ test/models_test/index_test.py
@@ -1,30 +1,39 @@
 from models import index_model
 
+real_student = {
+    'sid': '3',
+    'uni': 'ds9876'
+}
+fake_student = {
+    'sid': '-444',
+    'uni': 'qwerty'
+}
+
+real_teacher = {
+    'tid': '4'
+}
+fake_teacher = {
+    'tid': '-443',
+}
+
 def test_is_student(db):
-    real_student = {
-        'sid': '3',
-        'uni': 'ds9876'
-    }
-    im = index_model.Index(db, real_student['sid']) 
+    im = index_model.Index(db, real_student['sid'])
     assert im.is_student() is True
 
-    fake_student = {
-        'sid': '-444',
-        'uni': 'qwerty'
-    }
     im = index_model.Index(db, fake_student['sid'])
+    assert im.is_student() is False
+
+    im = index_model.Index(db, real_teacher['tid'])
     assert im.is_student() is False
 
 
 def test_is_teacher(db):
-    real_teacher = {
-        'tid': '4'
-    }
+
     im = index_model.Index(db, real_teacher['tid'])
     assert im.is_teacher() is True
 
-    fake_teacher = {
-        'tid': '-443',
-    }
     im = index_model.Index(db, fake_teacher['tid'])
+    assert im.is_teacher() is False
+
+    im = index_model.Index(db, real_student['sid'])
     assert im.is_teacher() is False
